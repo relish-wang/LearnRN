87d925817585e9b9fbbfd1d5f9fad36c31de0671
王鑫 <wangxin@souche.com>
2017-11-01 10:36:38 +0800

    feat: "更多页面"中使用MapView（react-native的MapView在RN的0.42版本中已彻底废弃；所以改用了react-native-maps的MapView）

--- ch04/more.js
+++ ch04/more.js
@@ -1,23 +1,22 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
+import {
+	View,
+	Text,
+  Dimensions
+} from 'react-native';
+import MapView from 'react-native-maps';
 
-export default class more extends React.Component{
-	render(){
+export default class more extends Component{
+
+	render() {
 		return (
-			<View style={styles.container}>
-				<ActivityIndicator color="purple" size="large" />
-			</View>
+			<MapView
+				initialRegion={{
+					latitude: 37.78825,
+					longitude: -122.4324,
+					latitudeDelta: 0.0922,
+					longitudeDelta: 0.0421
+				}} />
 		);
 	}
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	text: {
-		fontSize: 20
-	}
-})
