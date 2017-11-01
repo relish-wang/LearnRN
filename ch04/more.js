0049b02dcc0b3e46274657a2a74d1154cd7cb5e2
王鑫 <wangxin@souche.com>
2017-11-01 11:14:24 +0800

    feat: Switch的使用（对应Android的Switch/ToggleButton）

--- ch04/more.js
+++ ch04/more.js
@@ -3,32 +3,28 @@ import {
 	View,
 	StyleSheet,
 	Text,
-	Slider
+	Switch
 } from 'react-native';
 
 export default class more extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			sliderValue: 5
+			isOn: false
 		}
 	}
 
 	render() {
 		return (
 			<View style={styles.container}>
-				<Slider minimumValue={0}//最小值
-					style={{width: 200}}
-					step={1} //步长
-					maximumTrackTintColor='red'// 滑块右侧颜色
-					minimumTrackTintColor='blue'// 滑块左侧颜色
-					maximumValue={10}//最大值
-					value={this.state.sliderValue}
-					onValueChange={ (value) => {
-						this.setState({sliderValue: value})
-					}}
+				<Switch
+					onTintColor='blue'
+					thumbTintColor='green'
+					tintColor='black'
+					onValueChange={() => {this.setState({isOn: !this.state.isOn})}}
+					value={this.state.isOn === true}
 				/>
-				<Text>Slider值: {this.state.sliderValue}</Text>
+				<Text>Switch值: {this.state.isOn}</Text>
 			</View>
 		);
 	}
