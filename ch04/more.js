35f3a849f458be637b3d5aefe8984429fc344977
王鑫 <wangxin@souche.com>
2017-11-01 11:07:04 +0800

    feat: Slide的使用（对应Android的SeekBar）

--- ch04/more.js
+++ ch04/more.js
@@ -2,30 +2,33 @@ import React, {Component} from 'react';
 import {
 	View,
 	StyleSheet,
-	Picker,
 	Text,
-  Dimensions
+	Slider
 } from 'react-native';
-import MapView from 'react-native-maps';
 
 export default class more extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			language: 'java'
+			sliderValue: 5
 		}
 	}
 
 	render() {
 		return (
 			<View style={styles.container}>
-				<Picker
-					style={styles.picker}
-					selectedValue={this.state.language}
-					onValueChange={(lang) => this.setState({language: lang})}>
-						<Picker.Item label="Java" value="java" />
-						<Picker.Item label="JavaScript" value="javascript" />
-				</Picker>
+				<Slider minimumValue={0}//最小值
+					style={{width: 200}}
+					step={1} //步长
+					maximumTrackTintColor='red'// 滑块右侧颜色
+					minimumTrackTintColor='blue'// 滑块左侧颜色
+					maximumValue={10}//最大值
+					value={this.state.sliderValue}
+					onValueChange={ (value) => {
+						this.setState({sliderValue: value})
+					}}
+				/>
+				<Text>Slider值: {this.state.sliderValue}</Text>
 			</View>
 		);
 	}
