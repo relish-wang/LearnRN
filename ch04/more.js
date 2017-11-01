502bb9526daf4167e9808976fae1e9466b5fe723
王鑫 <wangxin@souche.com>
2017-11-01 10:52:15 +0800

    feat: Picker的使用

--- ch04/more.js
+++ ch04/more.js
@@ -1,22 +1,47 @@
 import React, {Component} from 'react';
 import {
 	View,
+	StyleSheet,
+	Picker,
 	Text,
   Dimensions
 } from 'react-native';
 import MapView from 'react-native-maps';
 
 export default class more extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			language: 'java'
+		}
+	}
 
 	render() {
 		return (
-			<MapView
-				initialRegion={{
-					latitude: 37.78825,
-					longitude: -122.4324,
-					latitudeDelta: 0.0922,
-					longitudeDelta: 0.0421
-				}} />
+			<View style={styles.container}>
+				<Picker
+					style={styles.picker}
+					selectedValue={this.state.language}
+					onValueChange={(lang) => this.setState({language: lang})}>
+						<Picker.Item label="Java" value="java" />
+						<Picker.Item label="JavaScript" value="javascript" />
+				</Picker>
+			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	text: {
+		fontSize: 20
+	},
+	picker: {
+		width: 200,
+		height: 200
+	}
+});
