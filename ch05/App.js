2970523e4b7efcd14f40ef9d4f8b9183b5c8ebd5
王鑫 <wangxin@souche.com>
2017-11-05 01:24:48 +0800

    fix: 用书上的方法不行，全局替换居然成功了？黑人问号？

--- ch05/App.js
+++ ch05/App.js
@@ -1,57 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components';
+import Main from './main';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class app extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+      <Navigator
+        initialRoute={{
+          name: 'main',
+          component: Main
+        }}
+        configureScene = {(route) => {
+            return Navigator.SceneConfigs.FloatFromRight;
+        }}
+        renderScene = {(route, navigator) => {
+          const Component = route.component;
+          return <Component {...route.params} navigator={navigator} />
+        }} />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
